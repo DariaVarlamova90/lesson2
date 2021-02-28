5ae042bf54275b96650cb2a257a1b60e7a4a153e
Daria <da.varlamova@yandex.ru>
2021-02-28 17:43:42 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+humans = [
+    { name: 'vvv', lastname: 'rrr' },
+    { name: 'sdf', lastname: 'sgrg' },
+    { name: '23423', lastname: 'f34r34' },
+];
+for (let i = 0; i < humans.length; i = i + 1) {
+    console.log(humans[i].name + ' ' + humans[i].lastname);
+
+};
+console.log('end');
